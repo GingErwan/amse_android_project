0606eb5483c6eba4bed34314ee16352f79610608
Ranto <ranto.rabesandratana@etu.imt-lille-douai.fr>
2021-03-05 16:00:15 +0100

    centrage des astéroides + début collision

--- app/src/main/java/fr/imt_lille_douai/amse_android_project/MainActivity.java
+++ app/src/main/java/fr/imt_lille_douai/amse_android_project/MainActivity.java
@@ -13,6 +13,7 @@ import android. os. Bundle;
 import android. app.Activity ;
 import android. view. Menu;
 import android. view. View;
+import android.view.ViewAnimationUtils;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
@@ -21,6 +22,9 @@ import android.animation.ObjectAnimator;
 import android.os.Bundle;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -35,8 +39,9 @@ public class MainActivity extends AppCompatActivity {
         ImageView img_asteroid2 = (ImageView)findViewById(R.id.img_asteroid2);
         ImageView img_asteroid3 = (ImageView)findViewById(R.id.img_asteroid3);
         ImageView img_asteroid4 = (ImageView)findViewById(R.id.img_asteroid4);
-        diagonalTranslation(img_asteroid1, 6000, "translationX", "translationY", 600);
-        translation(img_asteroid3, 3000, "translationY", 400);
+        List<ImageView> asteroidList = Arrays.asList(img_asteroid1, img_asteroid4, img_asteroid3, img_asteroid4);
+        diagonalTranslation(img_asteroid1, 6000, "translationX", "translationY", 700);
+        translation(img_asteroid3, 3000, "translationY", 1000);
         ellipse(img_asteroid2, 13000, 359f, 0f, 0f, 1000f, 1000f);
         ellipse(img_asteroid4, 10000, -359f, 50f, 100f, 700f, 700f);
         allRotations(img_asteroid1, 5000, img_asteroid2, 6000, img_asteroid3, 4000, img_asteroid4, 10000);
@@ -45,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void translation (View v, int duration, String type, float length) {
+        v.setY(200f);
         ObjectAnimator animation = ObjectAnimator.ofFloat(v, type, length);
         animation.setDuration(duration);
         animation.setRepeatCount(ValueAnimator.INFINITE);
@@ -54,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void diagonalTranslation (View v, int duration, String type1, String type2, float length) {
+        v.setY(300f);
         PropertyValuesHolder pvhX = PropertyValuesHolder.ofFloat(type1, length);
         PropertyValuesHolder pvhY = PropertyValuesHolder.ofFloat(type2, length);
         ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(v, pvhX, pvhY);
@@ -100,6 +107,20 @@ public class MainActivity extends AppCompatActivity {
         Rect rectSecondView = new Rect(secondPosition[0], secondPosition[1], secondPosition[0] + secondView.getMeasuredWidth(), secondPosition[1] + secondView.getMeasuredHeight());
 
         return(rectFirstView.intersect(rectSecondView));
+    }
+    public void animationCollision (ImageView firstView, ImageView secondView){
+        ImageView Explosion = (ImageView) findViewById(R.id.img_explosion);
+        /*ObjectAnimator animation = ObjectAnimator.ofFloat(Explosion, "alpha", 1f);*/
+        int explosionX = Explosion.getWidth()/2;
+        int explosionY = Explosion.getHeight()/2;
+        if (Collision(firstView, secondView)){
+            Explosion.setX(firstView.getX());
+            Explosion.setY(firstView.getY());
+            float finalRadius = (float) Math.hypot(explosionX, explosionY);
+            Animator animation = ViewAnimationUtils.createCircularReveal(Explosion, explosionX, explosionY, 0f, finalRadius);
+            Explosion.setVisibility(View.VISIBLE);
+            animation.start();
+        }
     }
 
 }
